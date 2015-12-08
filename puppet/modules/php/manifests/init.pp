be187376aa4c895d90fada92b3bf343be5410625
Florin Ercuș <florinutz@gmail.com>
2015-12-08 20:38:10 +0100

    switched to production env

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -4,7 +4,7 @@ class php {
     require => Package['php5-fpm'],
   }
 
-  package { [ 'php5-fpm', 'php5-cli', 'php5-intl', 'php5-gd', 'php5-mcrypt', 'php5-imagick', 'php5-redis', 'php5-mysql', 'php5-curl' ]:
+  package { [ 'php5-fpm', 'php5-cli', 'php5-intl', 'php5-gd', 'php5-mcrypt', 'php5-imagick', 'php5-redis', 'php5-mysql', 'php5-curl', 'php5-apcu' ]:
     ensure => present,
     require => Exec['apt-get update'],
   }
